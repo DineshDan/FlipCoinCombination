b2f0369ca120c6216217061517fece57f286688e
DineshDan <dineshdanchai341@gmail.com>
2020-11-24 12:05:41 +0530

    Added; Logic to check whether Heads or Tails

--- FlipcoinC.sh
+++ FlipcoinC.sh
@@ -1,2 +1,10 @@
-echo "Welcome flip coin combination program being solved in the master branch"
+echo "Welcome of the flip coin combination program being sloved in the Master branch"
+		random=$((RANDOM%2))
+		echo "$random"
+		if(($random==1))
+		then
+		     echo "Heads"
+		else
+		    echo "Tails"
+		fi
 
